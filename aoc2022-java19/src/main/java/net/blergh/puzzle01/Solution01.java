5a7b122920d573900d06262652b3e66a30a50d1a
Noah Yetter <nyetter@gmail.com>
2022-12-01 20:28:09 -0700

    Java solution to 1p2

--- aoc2022-java19/src/main/java/net/blergh/puzzle01/Solution01.java
+++ aoc2022-java19/src/main/java/net/blergh/puzzle01/Solution01.java
@@ -5,8 +5,10 @@ import net.blergh.Constants;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Solution01
 {
@@ -17,6 +19,12 @@ public class Solution01
         List<Elf> elves = Elf.elvesFromInputString(input01);
         Optional<Integer> maxCalories = elves.stream().map(e -> e.totalCalories()).max(Integer::compare);
         System.out.println("(p1 answer) elf with the most calories has: " + maxCalories.get()); // 72070
-    }
 
+        Integer topThreeTotal = elves.stream()
+                .map(e -> e.totalCalories())
+                .sorted(Comparator.reverseOrder())
+                .limit(3)
+                .reduce(0, Integer::sum);
+        System.out.println("(p2 answer) sum of top 3 calorie totals: " + topThreeTotal); // 211805
+    }
 }
