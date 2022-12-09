e1797b4b0f9047d3d38581ccf2177382d1751353
Noah Yetter <nyetter@gmail.com>
2022-12-08 22:10:05 -0800

    python solution to 9p2

--- python/puzzle09.py
+++ python/puzzle09.py
@@ -47,7 +47,7 @@ def move_tail(head, tail):
     return Point(x=tail.x+dx, y=tail.y+dy)
 
 
-def find_points_visited_by_tail(movements):
+def find_points_visited_by_tail_p1(movements):
     head = Point(x=0, y=0)
     tail = Point(x=0, y=0)
     visited_by_tail = {tail}
@@ -59,12 +59,30 @@ def find_points_visited_by_tail(movements):
     return visited_by_tail
 
 
+def find_points_visited_by_tail_p2(movements):
+    TAIL_COUNT = 9
+    head = Point(x=0, y=0)
+    tails = [Point(x=0, y=0) for _ in range(TAIL_COUNT)]
+    visited_by_tail = {Point(x=0, y=0)}
+    for direction, units in movements:
+        for _ in range(units):
+            head = move_head(head, direction)
+            tails[0] = move_tail(head, tails[0])
+            for i in range(1, TAIL_COUNT):
+                tails[i] = move_tail(tails[i-1], tails[i])
+            visited_by_tail.add(tails[-1])
+    return visited_by_tail
+
+
 if __name__ == "__main__":
     input09 = open("../input/input09").read()
 
     movements = movements_from_input(input09)
-    points_visited_by_tail = len(find_points_visited_by_tail(movements))
-    print(f"(p1 answer) How many positions does the tail of the rope visit at least once? {points_visited_by_tail}") # 6030
+    points_visited_by_tail_p1 = len(find_points_visited_by_tail_p1(movements))
+    print(f"(p1 answer) How many positions does the tail of the (1-knot) rope visit at least once? {points_visited_by_tail_p1}") # 6030
+
+    points_visited_by_tail_p2 = len(find_points_visited_by_tail_p2(movements))
+    print(f"(p2 answer) How many positions does the tail of the (10-knot) rope visit at least once? {points_visited_by_tail_p2}") # 2545
 
 
 ##############################
@@ -114,7 +132,7 @@ def test_move_tail():
     assert Point(x=0, y=0 == move_tail(head=Point(x=0, y=0), tail=Point(x=0, y=0)))
 
 
-def test_find_points_visited_by_tail():
+def test_find_points_visited_by_tail_p1():
     expected = set([
         Point(x=0, y=0),
         Point(x=1, y=0),
@@ -134,5 +152,22 @@ def test_find_points_visited_by_tail():
         Point(x=2, y=4),
         Point(x=3, y=4),
     ])
-    actual = find_points_visited_by_tail(movements_from_input(SAMPLE_INPUT))
+    actual = find_points_visited_by_tail_p1(movements_from_input(SAMPLE_INPUT))
+    assert expected == actual
+
+
+P2_SAMPLE_INPUT = """\
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+"""
+
+def test_find_points_visited_by_tail_p2():
+    expected = 36
+    actual = len(find_points_visited_by_tail_p2(movements_from_input(P2_SAMPLE_INPUT)))
     assert expected == actual
