94164fb890c0a9063161d5e94abe0419911e3c37
Noah Yetter <nyetter@gmail.com>
2022-12-14 21:54:20 -0700

    python solution to 14p2

--- python/puzzle14.py
+++ python/puzzle14.py
@@ -52,7 +52,7 @@ def move_sand(sand_point, grid) -> Point:
     return sand_point
 
 
-def simulate_sand_flow(grid): # destructive!
+def simulate_sand_flow_p1(grid): # destructive!
     SAND_ORIGIN = Point(x=500, y=0)
     LOWEST_ROCK_POINT = max([p.y for p in grid.keys()]) # Y-axis grows down so larger Y values are "lower" w/r/t gravity
 
@@ -74,13 +74,61 @@ def simulate_sand_flow(grid): # destructive!
             falling_sand = new_sand_point
 
 
+def simulate_sand_flow_p2(grid): # destructive!
+    SAND_ORIGIN = Point(x=500, y=0)
+    FLOOR_LEVEL = 2 + max([p.y for p in grid.keys()]) # Y-axis grows down so larger Y values are "lower" w/r/t gravity
+
+    # add the floor, it doesn't actually need to be infinitely wide, adding the height of the grid on either side will do
+    leftmost = min([p.x for p in grid.keys()])
+    rightmost = max([p.x for p in grid.keys()])
+    height = max([p.y for p in grid.keys()])
+    for floor_x in range(leftmost-height, rightmost+height+1):
+        grid[Point(x=floor_x, y=FLOOR_LEVEL)] = "#"
+
+    while True: # each sand unit
+        falling_sand = SAND_ORIGIN
+
+        while True: # each movement of a sand unit
+            new_sand_point = move_sand(falling_sand, grid)
+
+            if new_sand_point == falling_sand:
+                # sand came to rest
+                grid[new_sand_point] = "o"
+                if new_sand_point == SAND_ORIGIN:
+                    return
+                else:
+                    break
+
+            if new_sand_point.y >= FLOOR_LEVEL:
+                # woops
+                raise Exception(f"sand fell below the floor at {new_sand_point}, you have a bug")
+
+            falling_sand = new_sand_point        
+
+
+def print_grid(grid):
+    leftmost = min([p.x for p in grid.keys()])
+    rightmost = max([p.x for p in grid.keys()])
+    height = max([p.y for p in grid.keys()])
+
+    for y in range(0, height+1):
+        line = "".join([grid.get(Point(x,y), ".") for x in range(leftmost, rightmost+1)])
+        print(line)
+
+
 if __name__ == "__main__":
     input14 = open("../input/input14").read()
 
     grid = sparse_grid_from_input(input14)
-    simulate_sand_flow(grid)
+    simulate_sand_flow_p1(grid)
     sand_units = len([1 for k,v in grid.items() if v == "o"])
     print(f"(p1 answer) How many units of sand come to rest before sand starts flowing into the abyss below? {sand_units}") # 1298
+
+    grid = sparse_grid_from_input(input14)
+    simulate_sand_flow_p2(grid)
+    sand_units = len([1 for k,v in grid.items() if v == "o"])
+    print(f"(p2 answer) How many units of sand come to rest before sand starts flowing into the abyss below? {sand_units}") # 25585
+
 
 
 ##########################
@@ -133,7 +181,7 @@ def test_sparse_grid_from_input():
     assert expected == actual
 
 
-def test_simulate_sand_flow():
+def test_simulate_sand_flow_p1():
     expected = {
         Point(498,4): "#",
         Point(498,5): "#",
@@ -182,4 +230,77 @@ def test_simulate_sand_flow():
         Point(500,2): "o",
     }
     grid = sparse_grid_from_input(SAMPLE_INPUT)
-    simulate_sand_flow(grid)
+    simulate_sand_flow_p1(grid)
+    assert expected == grid
+    assert 24 == len([1 for k,v in grid.items() if v == "o"])
+
+
+def test_simulate_sand_flow_p2():
+    expected = {
+        Point(498,4): "#",
+        Point(498,5): "#",
+        Point(498,6): "#",
+        Point(497,6): "#",
+        Point(496,6): "#",
+        Point(503,4): "#",
+        Point(502,4): "#",
+        Point(502,5): "#",
+        Point(502,6): "#",
+        Point(502,7): "#",
+        Point(502,8): "#",
+        Point(502,9): "#",
+        Point(501,9): "#",
+        Point(500,9): "#",
+        Point(499,9): "#",
+        Point(498,9): "#",
+        Point(497,9): "#",
+        Point(496,9): "#",
+        Point(495,9): "#",
+        Point(494,9): "#",
+        #floor
+        Point(485,11): "#",
+        Point(486,11): "#",
+        Point(487,11): "#",
+        Point(488,11): "#",
+        Point(489,11): "#",
+        Point(490,11): "#",
+        Point(491,11): "#",
+        Point(492,11): "#",
+        Point(493,11): "#",
+        Point(494,11): "#",
+        Point(495,11): "#",
+        Point(496,11): "#",
+        Point(497,11): "#",
+        Point(498,11): "#",
+        Point(499,11): "#",
+        Point(500,11): "#",
+        Point(501,11): "#",
+        Point(502,11): "#",
+        Point(503,11): "#",
+        Point(504,11): "#",
+        Point(505,11): "#",
+        Point(506,11): "#",
+        Point(507,11): "#",
+        Point(508,11): "#",
+        Point(509,11): "#",
+        Point(510,11): "#",
+        Point(511,11): "#",
+        Point(512,11): "#",
+    }
+    expected.update({Point(x, 10): "o" for x in [490,491,492,493,494,502,503,504,505,506,507,508,509,510]})
+    expected.update({Point(x, 9): "o" for x in [491,492,493,503,504,505,506,507,508,509]})
+    expected.update({Point(x, 8): "o" for x in [492,493,494,495,496,497,498,499,500,501,503,504,505,506,507,508]})
+    expected.update({Point(x, 7): "o" for x in [493,494,495,496,498,499,500,501,503,504,505,506,507]})
+    expected.update({Point(x, 6): "o" for x in [494,495,499,500,501,503,504,505,506]})
+    expected.update({Point(x, 5): "o" for x in [495,496,497,499,500,501,503,504,505]})
+    expected.update({Point(x, 4): "o" for x in [496,497,499,500,501,504]})
+    expected.update({Point(x, 3): "o" for x in [497,498,499,500,501,502,503]})
+    expected.update({Point(x, 2): "o" for x in [498,499,500,501,502]})
+    expected.update({Point(x, 1): "o" for x in [499,500,501]})
+    expected.update({Point(x, 0): "o" for x in [500]})
+    
+    grid = sparse_grid_from_input(SAMPLE_INPUT)
+    simulate_sand_flow_p2(grid)
+    print_grid(grid)
+    assert expected == grid
+    assert 93 == len([1 for k,v in grid.items() if v == "o"])
