bce81697bf055ccb146de39e31bd0fe0631d8d5e
Noah Yetter <nyetter@gmail.com>
2022-12-18 18:30:53 -0700

    empty solution files and input files for 12, 16, 17

---
+++ python/puzzle12.py
@@ -0,0 +1,18 @@
+
+
+
+
+if __name__ == "__main__":
+    input12 = open("../input/input12").read()
+
+
+####################################
+
+SAMPLE_INPUT = """\
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+"""
+
---
+++ python/puzzle17.py
@@ -0,0 +1,13 @@
+
+if __name__ == "__main__":
+    input17 = open("../input/input17").read()
+    
+
+###########################
+
+SAMPLE_INPUT = """\
+"""
+
+
+def test_():
+    pass
